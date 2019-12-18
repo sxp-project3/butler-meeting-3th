343c54104b7df7a77bc014a433497d59ded06f61
jiefeimadi <865033445@qq.com>
2019-12-18 12:37:50 +0800

    工具类

---
+++ core/src/main/java/com/suixingpay/util/TokenUtil.java
@@ -0,0 +1,108 @@
+package com.suixingpay.util;
+
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.JWTVerifier;
+import com.auth0.jwt.algorithms.Algorithm;
+import com.auth0.jwt.interfaces.Claim;
+import com.auth0.jwt.interfaces.DecodedJWT;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import javax.servlet.http.HttpServletRequest;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * ClassName TokenUtil
+ *
+ * @Description TODO
+ * @Author luyun
+ * @Date 2019/12/18 10:55
+ * @Version 1.0
+ **/
+
+public class TokenUtil {
+    //token密钥
+    public static  final String SECRET="JKKLJOoasdlfj";
+
+    //token 过期时间
+    public  static  final  int calendarFiled= Calendar.DATE;
+    public  static  final  int calendarInterval=10;
+
+    @Autowired
+    HttpServletRequest httpServletRequest;
+
+    /**
+     * 功能描述: <JWT 生成token>
+     * 〈〉
+     * @Param: [userId, userName, userLevel]
+     * @Return: java.lang.String
+     * @Author: luyun
+     * @Date: 2019/12/18 11:50
+     */
+    public static  String creatToken(String userId,String userName,String userLevel) throws  Exception {
+        try {
+            Date iaDate = new Date();
+            //过期时间
+            Calendar nowTime = Calendar.getInstance();
+            nowTime.add(calendarFiled, calendarInterval);
+            Date expireTime = nowTime.getTime();
+            //私钥加密
+            Algorithm algorithm = Algorithm.HMAC256(SECRET);
+
+            //header.Map
+            Map<String, Object> header = new HashMap<>();
+            header.put("alg", "HS256");
+            header.put("typ", "JWT");
+
+            //build token
+            return JWT.create().withHeader(header)
+                    .withClaim("userId", userId)
+                    .withClaim("userName", userName)
+                    .withClaim("userLevel", userLevel)
+                    .withExpiresAt(expireTime)
+                    .sign(algorithm);
+        } catch (Exception e){
+            e.printStackTrace();
+        }
+        return  null;
+    }
+    /**
+     * 功能描述: <解密token>
+     * 〈〉
+     * @Param: [token]
+     * @Return: java.util.Map<java.lang.String,com.auth0.jwt.interfaces.Claim>
+     * @Author: luyun
+     * @Date: 2019/12/18 11:54
+     */
+    public static Map<String, Claim> verifyToken(String token){
+        DecodedJWT jwt=null;
+        try {
+            JWTVerifier jwtVerifier=JWT.require(Algorithm.HMAC256(SECRET)).build();
+            jwt=jwtVerifier.verify(token);
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+        return jwt.getClaims();
+    }
+    /**
+     * 功能描述: <根据token获取userId>
+     * 〈〉
+     * @Param: [token]
+     * @Return: int
+     * @Author: luyun
+     * @Date: 2019/12/18 12:00
+     */
+    public static int getId(String token){
+        Map<String,Claim> claimMap=new HashMap<>();
+        Claim userId=claimMap.get("userId");
+        if (Utils.isBlank(String.valueOf(userId))){
+            new RuntimeException("token异常 重新登录");
+        }
+        return Integer.valueOf(userId.asString());
+
+    }
+
+
+}
---
+++ core/src/main/java/com/suixingpay/util/Utils.java
@@ -0,0 +1,60 @@
+package com.suixingpay.util;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+import javax.servlet.http.HttpServletRequest;
+
+/**
+ * ClassName Utils
+ *
+ * @Description TODO
+ * @Author luyun
+ * @Date 2019/12/18 12:03
+ * @Version 1.0
+ **/
+
+public class Utils {
+
+    @Autowired
+     public static  HttpServletRequest httpServletRequest;
+
+    /**
+     * 功能描述: <判断是否为空>
+     * 〈〉
+     * @Param: [str]
+     * @Return: boolean
+     * @Author: luyun
+     * @Date: 2019/12/18 12:03
+     */
+    public static boolean isBlank(String str) {
+        int strLen;
+        if (str != null && (strLen = str.length()) != 0) {
+            for(int i = 0; i < strLen; ++i) {
+                if (!Character.isWhitespace(str.charAt(i))) {
+                    return false;
+                }
+            }
+
+            return true;
+        } else {
+            return true;
+
+        }
+    }
+    /**
+     * 功能描述: <根据token获取token值>
+     * 〈〉
+     * @Param: [tokenName]
+     * @Return: java.lang.String
+     * @Author: luyun
+     * @Date: 2019/12/18 12:05
+     */
+    public static  String getToken(String tokenName){
+        String token=httpServletRequest.getHeader(tokenName);
+        if (tokenName==null){
+            new RuntimeException("token为空，请登录");
+        }
+        return token;
+    }
+
+}
