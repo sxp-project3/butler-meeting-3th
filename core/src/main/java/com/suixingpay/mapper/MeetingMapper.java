f81a7c20c823c9734753339eb138eca515edbe6c
Zhang-LeYing <894843410@qq.com>
2019-12-19 15:50:16 +0800

    根据需求做的修改

--- core/src/main/java/com/suixingpay/mapper/MeetingMapper.java
+++ core/src/main/java/com/suixingpay/mapper/MeetingMapper.java
@@ -18,8 +18,12 @@ public interface MeetingMapper {
     //插入一条会议
     Integer insertMeeting(Meeting meeting);
 
+    //根据会议id修改会议信息
+    Integer updateMeetingById(Meeting meeting);
+
     // 获取我的可报名的会议
     List<Meeting> getListForFrontShow(@Param("nowDate") Date nowDate,
                                       @Param("createUserIds") List<Integer> createUserIds);
+
 //    @Param("mapParams") HashMap mapParams);
 }
