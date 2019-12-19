a5f182906aa524b6a7e26ba79ff5b46a9fd13eca
kongjian <298029530@qq.com>
2019-12-19 18:45:31 +0800

    增加demo-controller

---
+++ core/src/main/java/com/suixingpay/controller/MeetingDemoController.java
@@ -0,0 +1,96 @@
+package com.suixingpay.controller;
+
+import com.github.pagehelper.PageHelper;
+import com.github.pagehelper.PageInfo;
+import com.suixingpay.enumeration.CodeEnum;
+import com.suixingpay.pojo.Meeting;
+import com.suixingpay.response.Response;
+import com.suixingpay.service.MeetingKjService;
+import com.suixingpay.vo.SearchMeetingParamVo;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * @Author: kongjian
+ * @Date: 2019/12/18
+ */
+@RestController
+@RequestMapping("/meetingDemo")
+@Slf4j
+public class MeetingDemoController {
+    @Autowired
+    private MeetingKjService meetingKjService;
+
+    @RequestMapping(value = "/validMeetings", method = RequestMethod.GET)
+    public Response validMeeting(@RequestParam(value="pageNum", required = false) String pageNumString,
+                                 @RequestParam(value="pageSize", required = false) String pageSizeString) {
+        Integer userId = 10001; // 模拟用户id
+
+        Integer pageNum = Integer.parseInt(pageNumString);
+        Integer pageSize = Integer.parseInt(pageSizeString);
+        PageHelper.startPage(pageNum, pageSize);
+        List<Meeting> meetings = meetingKjService.getValidMeeting(userId);
+        PageInfo<Meeting> page = new PageInfo<>(meetings);
+//        Map<String, Object> result = new HashMap<>();
+//        result.put("meetings", meetings);
+        Response<Map<String, HashMap>> response = Response.getInstance(CodeEnum.SUCCESS, page);
+
+        return response;
+    }
+
+    @RequestMapping(value = "/detail", method = RequestMethod.GET)
+    public Response detail(@RequestParam(value = "id") String meetingIdString) {
+        // 这里尚未完成，需要验证用户身份
+        Integer userId = 10001; // 模拟用户id
+
+        Integer meetingId = Integer.parseInt(meetingIdString);
+        Meeting meeting = meetingKjService.getOne(meetingId);
+        Map<String, Object> result = new HashMap<>();
+        result.put("meeting", meeting);
+        Response<Map<String, HashMap>> response = Response.getInstance(CodeEnum.SUCCESS, result);
+
+        return response;
+    }
+
+    @RequestMapping(value = "/my-create-list")
+    public Response getMyCreateList(@RequestParam(value="pageNum", required = false) String pageNumString,
+                                    @RequestParam(value="pageSize", required = false) String pageSizeString) {
+        // 这里尚未完成，需要验证用户身份
+        Integer userId = 10001; // 模拟用户id
+
+        Integer pageNum = Integer.parseInt(pageNumString);
+        Integer pageSize = Integer.parseInt(pageSizeString);
+        PageHelper.startPage(pageNum, pageSize);
+        List<Meeting> meetings = meetingKjService.getValidMeeting(userId);
+        PageInfo<Meeting> page = new PageInfo<>(meetings);
+        Response<Map<String, HashMap>> response = Response.getInstance(CodeEnum.SUCCESS, page);
+
+        return response;
+    }
+
+    @RequestMapping(value = "/search-list", method = RequestMethod.POST)
+    public Response searchList(@RequestBody SearchMeetingParamVo searchMeetingParamVo) {
+        // 这里尚未完成，需要验证用户身份
+//        Integer userId = 10001; // 模拟用户id
+//
+//        Map<String,Object> params = new HashMap<>();
+//        params.put("createUserId", 10001);
+//        params.put("ifFee", 1);
+//        params.put("place", "北京");
+
+
+//        Integer pageNum = Integer.parseInt(pageNumString);
+//        Integer pageSize = Integer.parseInt(pageSizeString);
+//        PageHelper.startPage(pageNum, pageSize);
+        List<Meeting> meetings = meetingKjService.searchMeeting(searchMeetingParamVo);
+        PageInfo<Meeting> page = new PageInfo<>(meetings);
+        Response<Map<String, HashMap>> response = Response.getInstance(CodeEnum.SUCCESS, page);
+
+        return response;
+    }
+}
