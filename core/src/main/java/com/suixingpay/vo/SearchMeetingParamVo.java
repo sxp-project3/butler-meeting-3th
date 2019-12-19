2d4a7c6734dd2ca9e6fc63c032f32d011b8108da
kongjian <298029530@qq.com>
2019-12-19 18:38:05 +0800

    后台搜索demo

--- core/src/main/java/com/suixingpay/vo/SearchMeetingParamVo.java
+++ core/src/main/java/com/suixingpay/vo/SearchMeetingParamVo.java
@@ -45,6 +45,26 @@ public class SearchMeetingParamVo {
     // 创建用户id
     private Integer createUserId;
 
+    public Integer getPageNum() {
+        return pageNum;
+    }
+
+    public void setPageNum(Integer pageNum) {
+        this.pageNum = pageNum;
+    }
+
+    public Integer getPageSize() {
+        return pageSize;
+    }
+
+    public void setPageSize(Integer pageSize) {
+        this.pageSize = pageSize;
+    }
+
+    private Integer pageNum;
+
+    private Integer pageSize;
+
     public Integer getCreateUserId() {
         return createUserId;
     }
