6be6cbe199b3a5391b4fccfccc873ec53a28a784
jiefeimadi <865033445@qq.com>
2019-12-20 18:25:21 +0800

    对于异常的处理

--- core/src/main/java/com/suixingpay/util/TokenUtil.java
+++ core/src/main/java/com/suixingpay/util/TokenUtil.java
@@ -105,10 +105,10 @@ public class TokenUtil {
             JWTVerifier jwtVerifier=JWT.require(Algorithm.HMAC256(SECRET)).build();
              jwt=jwtVerifier.verify(token);
              jwt.getClaims();
-        }catch (Exception e){
-            throw new JWTDecodeException("token值错误");
-
-        }
+        }catch (JWTDecodeException e){
+            log.error("Token 值错误！");
+            return null;
+        } 
         Map<String, Object> map = new HashMap<>();
         int id=jwt.getClaims().get("id").asInt();
         String levelNum=jwt.getClaims().get("levelNum").asString();
