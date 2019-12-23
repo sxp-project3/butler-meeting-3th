3404882232d1719ee23845c3c9d03ea1d338b165
dsy1997 <duansiyu6@163.com>
2019-12-23 14:16:25 +0800

    更改需求

--- core/src/main/java/com/suixingpay/controller/SignController.java
+++ core/src/main/java/com/suixingpay/controller/SignController.java
@@ -366,7 +366,9 @@ public class SignController {
                 map.put("IsSignUp", sign1.getIsSignup());
                 if (sign1.getIsSignup() == 1 ){
                     String SignUpTime = sdf.format(sign1.getSignupTime());
-                    map.put("IsSignUp", SignUpTime);
+                    map.put("SignUpTime", SignUpTime);
+                }else if (sign1.getIsSignup() == 0){
+                    map.put("SignUpTime", null);
                 }
 
                 //通过用户id查询用户信息
@@ -414,14 +416,16 @@ public class SignController {
         log.info(meeting.getName()+"签到总人数"+signUpSum);
         //判断当前会议id是否存在
 
-        signUpVo.setPlaceProvince(meeting.getPlaceProvince());
-        signUpVo.setPlaceCity(meeting.getPlaceCity());
-        signUpVo.setSignUpSum(signUpSum);
+//        signUpVo.setPlaceProvince(meeting.getPlaceProvince());
+//        signUpVo.setPlaceCity(meeting.getPlaceCity());
+//        signUpVo.setSignUpSum(signUpSum);
 
         //通过会议id查询出当前会议下的所有用户id
         List<Integer> list = signService.selectIdByMeeting(sign);
 
         for (int i = 0;i < list.size();i++){
+
+//            List<SignUpVo> list1 = new ArrayList<>();
 
             Map<String, Object> map = new HashMap<>();
             //每一次获取到一个用户id去查询
@@ -445,8 +449,76 @@ public class SignController {
                 signUpVo.setReferralCode(butlerUser.getReferralCode());
                 signUpVo.setName(butlerUser.getName());
                 signUpVo.setTelephone(butlerUser.getTelephone());
+                signUpVo.setPlaceProvince(butlerUser.getProvince());
+                signUpVo.setPlaceCity(butlerUser.getCity());
 
                 list1.add(signUpVo);
+
+//                HSSFWorkbook wb = new HSSFWorkbook();
+//                HSSFSheet sheet = wb.createSheet("获取会议信息");
+//                HSSFRow row = null;
+//                row = sheet.createRow(0); //创建第一个单元格
+//                row.setHeight((short) (27 * 20));
+//                CellStyle cellStyle = wb.createCellStyle();
+//                row.createCell(0).setCellValue("报名信息"); //为第一行单元格设值
+//                //HSSFCell cell1 = row.createCell(0);
+//                cellStyle.setAlignment(HSSFCellStyle.ALIGN_CENTER);
+//                //cellStyle.setAlignment(HSSFCellStyle.ALIGN_CENTER);  //居中
+//                //合并单元格  CellRangeAddress(起始行号，终止行号， 起始列号，终止列号）
+//                CellRangeAddress rowRegion = new CellRangeAddress(0, 0, 0, 9);
+//                sheet.addMergedRegion(rowRegion);
+//                //设置表头
+//                row = sheet.createRow(1);
+//                row.setHeight((short) (23 * 20)); //设置行高
+//                row.createCell(0).setCellValue("推荐码"); //为第一个单元格设值
+//                row.createCell(1).setCellValue("手机号"); //为第二个单元格设值
+//                row.createCell(2).setCellValue("姓名");
+//                row.createCell(3).setCellValue("落地省");
+//                row.createCell(4).setCellValue("落地市");
+//                row.createCell(5).setCellValue("报名时间");
+//                row.createCell(6).setCellValue("是否参会签到");
+//
+//
+//                for (int j = 0; j < list1.size(); j++) {
+//                    row = sheet.createRow(j + 2);
+//                    row.createCell(0).setCellValue(signUpVo.getReferralCode());
+//                    row.createCell(1).setCellValue(signUpVo.getTelephone());
+//                    row.createCell(2).setCellValue(signUpVo.getName());
+//                    row.createCell(3).setCellValue(signUpVo.getPlaceProvince());
+//                    row.createCell(4).setCellValue(signUpVo.getPlaceCity());
+////            row.createCell(5).setCellValue(signUpVo.getSignupTime());
+////            row.createCell(6).setCellValue(signUpVo.getIsSignin());
+//
+//
+//
+//                    //   row.createCell(4).setCellValue(meeting.getMeetingStartTime());
+//                    //设置单元格时间格式
+//                    //  CellStyle cellStyle = wb.createCellStyle();
+//                    HSSFCell cell = row.createCell(5);
+//                    HSSFDataFormat format = wb.createDataFormat();
+//                    cellStyle.setDataFormat(format.getFormat("yyyy-MM-dd HH:mm:ss"));
+//                    cell.setCellValue(signUpVo.getSignupTime());
+//                    cell.setCellStyle(cellStyle);
+//
+//                    if (signUpVo.getIsSignin() == 1){
+//                        row.createCell(6).setCellValue("是");
+//                    }else if (signUpVo.getIsSignin() == 0){
+//                        row.createCell(6).setCellValue("否");
+//                    }
+//                }
+//                sheet.setDefaultRowHeight((short) (17 * 20));
+//                //列宽自适应
+//                for (int j = 0; j <= 9; j++) {
+//                    sheet.setColumnWidth(j, 20 * 256);
+//                }
+//
+//                response.setContentType("application/vnd.ms-excel;charset=utf-8");
+//                OutputStream os = response.getOutputStream();
+//                response.setHeader("Content-disposition", "attachment;filename=signUpInfo.xls"); //默认Excel名称
+//                wb.write(os);
+//                os.flush();
+//                os.close();
+
             }
         }
         HSSFWorkbook wb = new HSSFWorkbook();
@@ -517,8 +589,7 @@ public class SignController {
 
     }
 
-    }
-//    @RequestMapping(value = "/test",method = RequestMethod.POST)
+    //    @RequestMapping(value = "/test",method = RequestMethod.POST)
 //    public Sign test(){
 //
 //        Sign sign = new Sign();
@@ -540,7 +611,10 @@ public class SignController {
 //
 //        return Response.getInstance(CodeEnum.SUCCESS,"查询成功");
 //
-//    }
+    }
+
+
+
 
 
 
